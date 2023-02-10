9ae705d43bbf4846a8c3a2f299763362bc259937
max-mahmud <newmahmud24@gmail.com>
2023-02-09 21:26:59 -0800

    after done

--- src/component/Gallary/Gall.js
+++ src/component/Gallary/Gall.js
@@ -5,7 +5,7 @@ const Moment = ({image}) => {
         <>
         <div className="col-lg-3 col-md-6 mb-4">
                 <img src={image} alt="img" className='w-100'/>
-                </div>
+            </div>
         </>
     )
 }
