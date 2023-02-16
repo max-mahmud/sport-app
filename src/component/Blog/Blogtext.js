7901fc96df9ed669712b5e3cbb00270e1326430d
max-mahmud <newmahmud24@gmail.com>
2023-02-15 20:39:26 -0800

    done

--- src/component/Blog/Blogtext.js
+++ src/component/Blog/Blogtext.js
@@ -1,32 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import './Blog.css'
 
 const Blogtext = () => {
+  const [blog, setBlog] = useState([])
+  useEffect(()=>{
+    fetch('blog.json')
+    .then (res => res.json())
+    .then (data => setBlog(data))
+  },[])
   return (
     <>
       <div className="blog_area">
         <Container>
             <h2 className='blog-heading'>Questions Answer</h2>
             <Row>
-                <Col lg={4} >
+              {
+                blog.map((item)=>
+                  <Col key={item.id} lg={4} >
                     <div className="single_blog">
-                    <h4>Difference between authorization and authentication</h4>
-                    <p>Authentication is a process to verify any person but Authorization is a process to the person access level. Authentication is verifying any person. Authorization is set the person's activity. Authentication active through Email, SMS, One time pin etc. Authorization can set using settings. User can set the Authentication settings but user can't set the Authorization settings, it can set by the organization or admin.</p>
+                    <h4>{item.heading} </h4>
+                    <p>{item.text}</p>
                     </div>
                 </Col>
-                <Col lg={4}>
-                    <div className="single_blog">
-                    <h4>Why I'm using firebase? What other options I have to implement authentication?</h4>
-                    <p>I'm using Firebase because is's safe and fast. The trust level is very high because of Google's service. Also we can start Firebase free. Also the documentation is well described. Using Firebase we can do Authentication, Realtime Database, Hosting and many more in one place easily. We can use Email, Password authentication, Google, Facebook, Apple, Github authentication and many more.</p>
-                    </div>
-                </Col>
-                <Col lg={4}>
-                    <div className="single_blog">
-                    <h4>What other services does firebase provide other than authentication.</h4>
-                    <p>Other than authentication, firebase have some other services. Firebase provide Realtime Database, Hosting, Cloud Storage, Google Analytics, Cloud Firestore, Cloud Messaging, Cloud Functions etc. They have free and paid both services. Their services are used for different platform like, Web, IOS+, Android, Unity etc. They have Machine Learning package too.</p>
-                    </div>
-                </Col>
+                )
+              }
             </Row>
         </Container>
       </div>
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import Logo from "../../image/logo.png"
 import './Header.css'
 
@@ -12,13 +12,13 @@ const Header = () => {
         <header className='myheader'>
         <Navbar bg="dark" expand="lg" variant="dark" className='py-3 fixed-top'>
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand as={Link} to="/">
         <img src={Logo} alt="Img" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <NavLink className="nav-link active-link" to="/">Home</NavLink>
+            <NavLink className="nav-link active-link" to="/home">Home</NavLink>
             <NavLink className="nav-link" to="/about">About</NavLink>
             <NavLink className="nav-link" to="/blog">Blog</NavLink>
             <NavLink className="nav-link" to="/">Login</NavLink>
